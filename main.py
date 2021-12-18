6fb3ede41a9c6e298a6b61962af74049a4c5d448
Ishan Karmakar <ishan.karmakar24@gmail.com>
2021-12-18 13:48:40 -0600

    Put error handling

--- main.py
+++ main.py
@@ -13,68 +13,72 @@ load_dotenv()
 import os
 
 def bcad(driver, house):
-    print("Searching in BCAD")
-    driver.get('https://esearch.brazoscad.org')
-    WebDriverWait(driver, 10).until(EC.presence_of_element_located((By.ID, "keywords")))
-    textbox = driver.find_element(By.ID, "keywords")
-    textbox.send_keys(house)
-    textbox.send_keys(Keys.RETURN)
-    print(f'Searching for {house}')
     try:
-        WebDriverWait(driver, 60).until(EC.element_to_be_clickable((By.ID, "view-list")))
-    except:
-        pass
-    finally:
+        print("Searching in BCAD")
+        driver.get('https://esearch.brazoscad.org')
+        WebDriverWait(driver, 10).until(EC.presence_of_element_located((By.ID, "keywords")))
+        textbox = driver.find_element(By.ID, "keywords")
+        textbox.send_keys(house)
+        textbox.send_keys(Keys.RETURN)
+        print(f'Searching for {house}')
+        WebDriverWait(driver, 30).until(EC.element_to_be_clickable((By.ID, "view-list")))
         driver.find_element(By.ID, "view-list").click()
-    WebDriverWait(driver, 10).until(EC.element_to_be_clickable((By.XPATH, f"//a[contains(text(), '{house.upper()}')]")))
-    driver.find_element(By.XPATH, f"//a[contains(text(), '{house.upper()}')]").click()
-    print('Clicked on link')
-    WebDriverWait(driver, 10).until(EC.presence_of_element_located((By.XPATH, "//tr/th[contains(text(), 'Assessed Value')]/following-sibling::td")))
-    assessed_value = driver.find_element(By.XPATH, "//tr/th[contains(text(), 'Assessed Value')]/following-sibling::td").text
-    print("Got assessed value")
-    WebDriverWait(driver, 5).until(EC.presence_of_element_located((By.XPATH, "//tr/th[contains(text(), 'Appraised Value')]/following-sibling::td")))
-    appraised_value = driver.find_element(By.XPATH, "//tr/th[contains(text(), 'Appraised Value')]/following-sibling::td").text
-    print("Got appraised value")
-    WebDriverWait(driver, 5).until(EC.presence_of_element_located((By.XPATH, "//strong[contains(text(), 'Estimated Taxes Without Exemptions: ')]")))
-    tax = driver.find_element(By.XPATH, "//strong[contains(text(), 'Estimated Taxes Without Exemptions: ')]").find_element(By.XPATH, "..").text.split('Estimated Taxes Without Exemptions: ')[1]
-    print("Got tax")
-    return assessed_value, appraised_value, tax
+        WebDriverWait(driver, 10).until(EC.element_to_be_clickable((By.XPATH, f"//a[contains(text(), '{house.upper()}')]")))
+        driver.find_element(By.XPATH, f"//a[contains(text(), '{house.upper()}')]").click()
+        print('Clicked on link')
+        WebDriverWait(driver, 10).until(EC.presence_of_element_located((By.XPATH, "//tr/th[contains(text(), 'Assessed Value')]/following-sibling::td")))
+        assessed_value = driver.find_element(By.XPATH, "//tr/th[contains(text(), 'Assessed Value')]/following-sibling::td").text
+        print("Got assessed value")
+        WebDriverWait(driver, 5).until(EC.presence_of_element_located((By.XPATH, "//tr/th[contains(text(), 'Appraised Value')]/following-sibling::td")))
+        appraised_value = driver.find_element(By.XPATH, "//tr/th[contains(text(), 'Appraised Value')]/following-sibling::td").text
+        print("Got appraised value")
+        WebDriverWait(driver, 5).until(EC.presence_of_element_located((By.XPATH, "//strong[contains(text(), 'Estimated Taxes Without Exemptions: ')]")))
+        tax = driver.find_element(By.XPATH, "//strong[contains(text(), 'Estimated Taxes Without Exemptions: ')]").find_element(By.XPATH, "..").text.split('Estimated Taxes Without Exemptions: ')[1]
+        print("Got tax")
+        return assessed_value, appraised_value, tax
+    except:
+        print("Error, Retrying")
+        return bcad(driver, house)
 
 def wcad(driver, house):
-    print("Searching in WCAD")
-    driver.get('https://search.wcad.org/')
-    WebDriverWait(driver, 10).until(EC.presence_of_element_located((By.ID, "SearchText")))
-    textbox = driver.find_element(By.ID, "SearchText")
-    textbox.send_keys(house)
-    textbox.send_keys(Keys.RETURN)
-    print(f'Searching for {house}')
-    WebDriverWait(driver, 20).until(EC.presence_of_element_located((By.XPATH, "//td[contains(text(), '{}')]".format(house.upper()))))
-    driver.find_element(By.XPATH, "//td[contains(text(), '{}')]".format(house.upper())).click()
-    print('Clicked on link')
-    WebDriverWait(driver, 20).until(EC.presence_of_element_located((By.ID, "dnn_ctr1460_View_ddTaxYears")))
-    dropdown = Select(driver.find_element(By.ID, "dnn_ctr1460_View_ddTaxYears"))
-    dropdown.select_by_visible_text('2021')
-    WebDriverWait(driver, 10).until(EC.presence_of_element_located((By.ID, "dnn_ctr1460_View_tdVITotalAppraisedValue")))
-    appraised_value = driver.find_element(By.ID, "dnn_ctr1460_View_tdVITotalAppraisedValue").text
-    print("Got appraised value")
-    WebDriverWait(driver, 5).until(EC.presence_of_element_located((By.ID, "dnn_ctr1460_View_tdVITotalAssessedValueRP")))
-    assessed_value = driver.find_element(By.ID, "dnn_ctr1460_View_tdVITotalAssessedValueRP").text
-    print("Got assessed value")
-    WebDriverWait(driver, 5).until(EC.element_to_be_clickable((By.ID, "tdDropDownLinks")))
-    driver.find_element(By.ID, "tdDropDownLinks").click()
-    WebDriverWait(driver, 5).until(EC.element_to_be_clickable((By.XPATH, "//a[contains(text(), 'Tax Office')]")))
-    driver.find_element(By.XPATH, "//a[contains(text(), 'Tax Office')]").click()
-    print("Clicked on tax office")
-    sleep(1)
-    driver.switch_to.window(driver.window_handles[1])
-    WebDriverWait(driver, 20).until(EC.presence_of_element_located((By.ID, "dnn_ctr377_View_tdPMCurrentAmountDue")))
-    tax = ''
-    while not tax:
-        tax = driver.find_element(By.ID, 'dnn_ctr377_View_tdPMCurrentAmountDue').text
-    print("Got tax")
-    driver.close()
-    driver.switch_to.window(driver.window_handles[0])
-    return appraised_value, assessed_value, tax
+    try:
+        print("Searching in WCAD")
+        driver.get('https://search.wcad.org/')
+        WebDriverWait(driver, 10).until(EC.presence_of_element_located((By.ID, "SearchText")))
+        textbox = driver.find_element(By.ID, "SearchText")
+        textbox.send_keys(house)
+        textbox.send_keys(Keys.RETURN)
+        print(f'Searching for {house}')
+        WebDriverWait(driver, 20).until(EC.presence_of_element_located((By.XPATH, "//td[contains(text(), '{}')]".format(house.upper()))))
+        driver.find_element(By.XPATH, "//td[contains(text(), '{}')]".format(house.upper())).click()
+        print('Clicked on link')
+        WebDriverWait(driver, 20).until(EC.presence_of_element_located((By.ID, "dnn_ctr1460_View_ddTaxYears")))
+        dropdown = Select(driver.find_element(By.ID, "dnn_ctr1460_View_ddTaxYears"))
+        dropdown.select_by_visible_text('2021')
+        WebDriverWait(driver, 10).until(EC.presence_of_element_located((By.ID, "dnn_ctr1460_View_tdVITotalAppraisedValue")))
+        appraised_value = driver.find_element(By.ID, "dnn_ctr1460_View_tdVITotalAppraisedValue").text
+        print("Got appraised value")
+        WebDriverWait(driver, 5).until(EC.presence_of_element_located((By.ID, "dnn_ctr1460_View_tdVITotalAssessedValueRP")))
+        assessed_value = driver.find_element(By.ID, "dnn_ctr1460_View_tdVITotalAssessedValueRP").text
+        print("Got assessed value")
+        WebDriverWait(driver, 5).until(EC.element_to_be_clickable((By.ID, "tdDropDownLinks")))
+        driver.find_element(By.ID, "tdDropDownLinks").click()
+        WebDriverWait(driver, 5).until(EC.element_to_be_clickable((By.XPATH, "//a[contains(text(), 'Tax Office')]")))
+        driver.find_element(By.XPATH, "//a[contains(text(), 'Tax Office')]").click()
+        print("Clicked on tax office")
+        sleep(1)
+        driver.switch_to.window(driver.window_handles[1])
+        WebDriverWait(driver, 20).until(EC.presence_of_element_located((By.ID, "dnn_ctr377_View_tdPMCurrentAmountDue")))
+        tax = ''
+        while not tax:
+            tax = driver.find_element(By.ID, 'dnn_ctr377_View_tdPMCurrentAmountDue').text
+        print("Got tax")
+        driver.close()
+        driver.switch_to.window(driver.window_handles[0])
+        return appraised_value, assessed_value, tax
+    except:
+        print("Error, Retrying")
+        return wcad(driver, house)
 
 db = sqlite3.connect(os.getenv('DATABASE_LOCATION'))
 cursor = db.cursor()
