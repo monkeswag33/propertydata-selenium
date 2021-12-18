7183ee56f506e6d7b400e199d19d785b35cb25f7
Ishan Karmakar <ishan.karmakar24@gmail.com>
2021-12-18 16:21:43 -0600

    Update names of houses

--- init_db.sql
+++ init_db.sql
@@ -13,13 +13,13 @@ CREATE TABLE IF NOT EXISTS "propertydata" (
 	PRIMARY KEY("id" AUTOINCREMENT)
 );
 INSERT INTO propertydata (name, cad) VALUES
-('212 Cloud', 'w'), ('118 Brown', 'w'),
-('103 Axis Deer', 'w'), ('105 Orchard', 'w'),
-('6018 Andross', 'w'), ('118 Wegstrom', 'w'),
-('313 Ross', 'w'), ('302 Quail', 'w'),
-('403 Kates', 'w'), ('107 Orchard', 'w'),
-('116 Sylvan', 'w'), ('128 Flinn', 'w'),
-('900 Stewart', 'w'),
+('212 Cloud RD', 'w'), ('118 Brown ST', 'w'),
+('103 Axis Deer TRL', 'w'), ('105 Orchard WAY', 'w'),
+('6018 Andross CT', 'w'), ('118 Wegstrom ST', 'w'),
+('313 Ross ST', 'w'), ('302 Quail CIR', 'w'),
+('403 Kates WAY', 'w'), ('107 Orchard WAY', 'w'),
+('116 Sylvan ST', 'w'), ('128 Flinn ST', 'w'),
+('900 Stewart DR', 'w'),
 ('802 San Pedro', 'b'), ('800 San Pedro', 'b'),
 ('706 San Pedro', 'b'), ('704 San Pedro', 'b'),
 ('702 San Pedro', 'b'), ('700 San Pedro', 'b');
