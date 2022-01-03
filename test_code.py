76b7866e5ec1d4f85c034d199146e159218fb75c
Ishan Karmakar <ishan.karmakar24@gmail.com>
2022-01-02 21:03:09 -0600

    Add tests

---
+++ test_code.py
@@ -0,0 +1,53 @@
+from main import wcad, bcad, fmv, init_programs
+import pytest
+
+driver, db, cursor, client = init_programs()
+houses = {
+    'b': [
+        "700 San Pedro",
+        "702 San Pedro",
+        "704 San Pedro",
+        "706 San Pedro",
+        "800 San Pedro",
+        "802 San Pedro"
+    ],
+    'w': [
+        "103 Axis Deer TRL",
+        "105 Orchard WAY",
+        "107 Orchard WAY",
+        "116 Sylvan ST",
+        "118 Brown ST",
+        "118 Wegstrom ST",
+        "128 Flinn ST",
+        "212 Cloud RD",
+        "302 Quail CIR",
+        "313 Ross ST",
+        "403 Kates WAY",
+        "6018 Andross CT",
+        "900 Stewart DR"
+    ]
+}
+
+@pytest.mark.parametrize("house", houses['b'])
+def test_bcad(house):
+    assessed_appraised_value = {
+        'assessed_value': None,
+        'appraised_value': None,
+        'tax': None
+    }
+    bcad(driver, house, assessed_appraised_value, 5)
+    assert None not in assessed_appraised_value.values()
+
+@pytest.mark.parametrize("house", houses['w'])
+def test_wcad(house):
+    assessed_appraised_value = {
+        'assessed_value': None,
+        'appraised_value': None,
+        'tax': None
+    }
+    wcad(driver, house, assessed_appraised_value, 5)
+    assert None not in assessed_appraised_value.values()
+
+@pytest.mark.parametrize("house", houses['w'])
+def test_fmv(house):
+    print(house)
